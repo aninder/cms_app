4816e9c21cf0ddf5270d2065735d6fa534d03f13
aninder <aninder@gmail.com>
2015-04-12 01:10:39 +0530

    scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KomalWebsite::Application.config.secret_token = '3afaf26aae64e5372b0d52ffb3060754860566499d6e3f8d33adf31449267dadb3323368d805b98794caec3ad840b9712b2c57e886ce56a7ff90678fb5265c1c'
